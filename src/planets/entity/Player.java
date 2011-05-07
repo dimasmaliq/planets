a672dd99d03c581f427f2fbe38bb5f83235b0f44
carlos.at.home@gmail.com <carlos.at.home@gmail.com@4b3cd46c-2daa-8291-0b90-90ad9a206fe4>
2011-05-07 15:45:01 +0000

    Variable speed rotation and movement for the player

--- src/planets/entity/Player.java
+++ src/planets/entity/Player.java
@@ -16,6 +16,36 @@ import planets.Entity;
  */
 public class Player extends Entity implements Moveable
 {
+    /**
+     * How fast the player moves
+     */
+    private float speed;
+
+    /**
+     * How fast the player rotates
+     */
+    private float turnSpeed;
+
+    /**
+     * A good speed for the player to move at the start
+     */
+    private static final float DEFAULT_SPEED = .10f;
+
+    /**
+     * A good speed for the player to turn at the start
+     */
+    private static final float DEFAULT_TURNSPEED = 1.0f;
+
+    /**
+     * Make a new player
+     */
+    public Player()
+    {
+        super();
+        speed = DEFAULT_SPEED;
+        turnSpeed = DEFAULT_TURNSPEED;
+    }
+
     public void update()
     {
         handleInput();
@@ -47,11 +77,11 @@ public class Player extends Entity implements Moveable
          */
         if(TestableKeyboard.isKeyDown(Keyboard.KEY_W))
         {
-            move(new Vector3f(0.0f, .10f, 0.0f));
+            move(new Vector3f(0.0f, speed, 0.0f));
         }
         else if(TestableKeyboard.isKeyDown(Keyboard.KEY_S))
         {
-            move(new Vector3f(0.0f, -.10f, 0.0f));
+            move(new Vector3f(0.0f, -speed, 0.0f));
         }
 
         /**
@@ -59,11 +89,11 @@ public class Player extends Entity implements Moveable
          */
         if(TestableKeyboard.isKeyDown(Keyboard.KEY_A))
         {
-            move(new Vector3f(-.10f, 0.0f, 0.0f));
+            move(new Vector3f(-speed, 0.0f, 0.0f));
         }
         else if(TestableKeyboard.isKeyDown(Keyboard.KEY_D))
         {
-            move(new Vector3f(.10f, 0.0f, 0.0f));
+            move(new Vector3f(speed, 0.0f, 0.0f));
         }
 
         /**
@@ -71,11 +101,11 @@ public class Player extends Entity implements Moveable
          */
         if(TestableKeyboard.isKeyDown(Keyboard.KEY_Q))
         {
-            move(new Vector3f(0.0f, 0.0f, -.10f));
+            move(new Vector3f(0.0f, 0.0f, -speed));
         }
         else if(TestableKeyboard.isKeyDown(Keyboard.KEY_E))
         {
-            move(new Vector3f(0.0f, 0.0f, .10f));
+            move(new Vector3f(0.0f, 0.0f, speed));
         }
 
         /**
@@ -83,11 +113,11 @@ public class Player extends Entity implements Moveable
          */
         if(TestableKeyboard.isKeyDown(Keyboard.KEY_LEFT))
         {
-            rotate(new Vector3f(0.0f, -.10f, 0.0f));
+            rotate(new Vector3f(0.0f, -turnSpeed, 0.0f));
         }
         else if(TestableKeyboard.isKeyDown(Keyboard.KEY_RIGHT))
         {
-            rotate(new Vector3f(0.0f, 0.10f, 0.0f));
+            rotate(new Vector3f(0.0f, turnSpeed, 0.0f));
         }
 
         /**
@@ -95,11 +125,29 @@ public class Player extends Entity implements Moveable
          */
         if(TestableKeyboard.isKeyDown(Keyboard.KEY_UP))
         {
-            rotate(new Vector3f(-.10f, 0.0f, 0.0f));
+            rotate(new Vector3f(-turnSpeed, 0.0f, 0.0f));
         }
         else if(TestableKeyboard.isKeyDown(Keyboard.KEY_DOWN))
         {
-            rotate(new Vector3f(0.10f, 0.0f, 0.0f));
+            rotate(new Vector3f(turnSpeed, 0.0f, 0.0f));
         }
+    }
+
+    /**
+     * Gets how fast the player can move
+     * @return the speed at which the player moves
+     */
+    public float getSpeed()
+    {
+        return speed;
+    }
+
+    /**
+     * Get how fast the player can turn
+     * @return the speed at which the player turns
+     */
+    public float getTurnSpeed()
+    {
+        return turnSpeed;
     }
 }
